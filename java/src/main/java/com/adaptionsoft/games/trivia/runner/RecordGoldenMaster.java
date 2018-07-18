36fed948e07327691eec59561864b26e50b1d16f
Tomasz Janowski <tomasz.janowski@rea-group.com>
2018-07-18 23:34:53 +1000

    record golden master

---
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/RecordGoldenMaster.java
@@ -0,0 +1,30 @@
+
+package com.adaptionsoft.games.trivia.runner;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.PrintStream;
+import java.util.stream.IntStream;
+
+public class RecordGoldenMaster {
+
+	private static void doSomething(Integer i) {
+		PrintStream orgStream = System.out;
+		try(PrintStream fileStream = new PrintStream(new FileOutputStream("src/test/resources/golden-master/output-" + i + ".txt", true))) {
+			// Redirecting console output to file
+			System.setOut(fileStream);
+			GameRunner.main(new String[]{Integer.toString(i)});
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} finally {
+			System.setOut(orgStream);
+		}
+}
+
+
+	public static void main(String[] args) {
+		IntStream.range(1, 10).forEach(RecordGoldenMaster::doSomething);
+
+	}
+}
