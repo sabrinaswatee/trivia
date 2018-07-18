e1df184f1185a57d46874d68184f93265bda6b1d
Tomasz Janowski <tomasz.janowski@rea-group.com>
2018-07-18 22:55:35 +1000

    Passing an argument for seed value

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -9,28 +9,35 @@ public class GameRunner {
 
 	private static boolean notAWinner;
 
+	private static long getSeed(String[] args) {
+		if (args.length == 0) {
+			return 0L;
+		} else {
+			return Long.parseLong(args[0]);
+		}
+	}
 	public static void main(String[] args) {
 		Game aGame = new Game();
-		
+
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
-		Random rand = new Random();
-	
+
+		Random rand = new Random(getSeed(args));
+
 		do {
-			
+
 			aGame.roll(rand.nextInt(5) + 1);
-			
+
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
+
+
+
 		} while (notAWinner);
-		
+
 	}
 }
